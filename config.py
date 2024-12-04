06f4b2a3bf576908453771a8b85ea1b95f0b747e
jimmy <jimmy071919@gmail.com>
2024-12-04 11:21:53 +0800

    修改安裝FFMPEG

--- config.py
+++ config.py
@@ -11,13 +11,20 @@ TENOR_API_URL = "https://tenor.googleapis.com/v2/search"
 # 路徑配置
 import platform
 import os
+import shutil
 
-# 根據操作系統設定 FFMPEG 路徑
-if platform.system() == 'Windows':
-    FFMPEG_PATH = "C:\\Program Files\\ffmpeg-7.1-full_build\\bin\\ffmpeg.exe"
-else:
-    # Linux 環境（如 Railway）使用系統級 FFMPEG
-    FFMPEG_PATH = "ffmpeg"
+# 獲取 FFMPEG 路徑
+def get_ffmpeg_path():
+    if platform.system() == 'Windows':
+        return "C:\\Program Files\\ffmpeg-7.1-full_build\\bin\\ffmpeg.exe"
+    else:
+        # 在 Linux 上尋找 ffmpeg
+        ffmpeg_path = shutil.which('ffmpeg')
+        if ffmpeg_path:
+            return ffmpeg_path
+        return 'ffmpeg'  # 如果找不到，使用默認路徑
+
+FFMPEG_PATH = get_ffmpeg_path()
 
 # 檔案路徑
 EMOJI_DATA_PATH = "emoji_data.json"
